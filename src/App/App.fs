09fc254364ea2607e127f6406abf84634aa3d7df
David Dawkins <david.dawkins@gmail.com>
2023-01-30 17:26:11 +0000

    dotnet 6 Sutil 2.* tailwind 3.2.4

--- src/App/App.fs
+++ src/App/App.fs
@@ -1,15 +1,12 @@
 module App
 
 open Sutil
-open Sutil.DOM
-open Sutil.Attr
 
 // For the best Tailwind experience, install the 'Tailwind CSS IntelliSense' extension.
 // See README.md for full details.
 
 let view() =
-    Html.div [
-        class' "text-center text-6xl p-6"
+    Html.divc "text-center text-6xl p-6" [
         text "Hello Tailwind World"
     ]
 
